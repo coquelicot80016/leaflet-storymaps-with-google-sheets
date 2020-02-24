bcd2c46fffa1fcdbd54bf25d85a62fa1dc991e67
coquelicot80016 <61419154+coquelicot80016@users.noreply.github.com>
2020-02-24 14:56:31 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1CIHHhJk8WmPip-taA6YmXA-Rzxku4GdwKG3G2CSGPE4/edit#gid=0';
